7cb4d3856310a7e9e2df240a67ea19ecd34f4e1d
Ubuntu <ubuntu@ip-172-31-10-99.ec2.internal>
2022-03-03 07:08:21 +0000

    serve

--- serve_model.py
+++ serve_model.py
@@ -1,3 +1,4 @@
+from functools import partial
 import os
 import gc
 import random
@@ -26,8 +27,22 @@ from nltk.tokenize import word_tokenize
 import numpy as np
 import json
 
-# read the command line options
-params = options.readCommandLine()
+import redis
+from flask import Flask, request
+from flask_cors import CORS
+
+import pickle as pkl
+import time
+import multiprocessing as mp
+import requests
+import re
+
+Q = None
+
+app = Flask(__name__)
+CORS(app)
+
+r = redis.Redis(host='localhost', port=6379, db=1)
 
 # seed rng for reproducibility
 # manualSeed = 1234
@@ -36,89 +51,106 @@ params = options.readCommandLine()
 # if params['useGPU']:
 #     torch.cuda.manual_seed_all(manualSeed)
 
-# setup dataloader
-dlparams = params.copy()
-dlparams['useIm'] = True
-dlparams['useHistory'] = True
-dlparams['numRounds'] = 10
-splits = ['val']
+params, info, dataset, qBot, aBot = None, None, None, None, None
 
-dataset = VisDialDataset(dlparams, splits)
+def load_objects():
+    # read the command line options
+    params = options.readCommandLine()
 
-# Transferring dataset parameters
-transfer = ['vocabSize', 'numOptions', 'numRounds']
-for key in transfer:
-    if hasattr(dataset, key):
-        params[key] = getattr(dataset, key)
+    # setup dataloader
+    dlparams = params.copy()
+    dlparams['useIm'] = True
+    dlparams['useHistory'] = True
+    dlparams['numRounds'] = 10
+    splits = ['val']
 
-if 'numRounds' not in params:
-    params['numRounds'] = 10
+    dataset = VisDialDataset(dlparams, splits)
 
-# Always load checkpoint parameters with continue flag
-params['continue'] = True
+    # Transferring dataset parameters
+    transfer = ['vocabSize', 'numOptions', 'numRounds']
+    for key in transfer:
+        if hasattr(dataset, key):
+            params[key] = getattr(dataset, key)
 
-excludeParams = ['batchSize', 'visdomEnv', 'startFrom', 'qstartFrom', 'trainMode', \
-    'evalModeList', 'inputImg', 'inputQues', 'inputJson', 'evalTitle', 'beamSize', \
-    'enableVisdom', 'visdomServer', 'visdomServerPort']
+    if 'numRounds' not in params:
+        params['numRounds'] = 10
 
-aBot = None
-qBot = None
+    # Always load checkpoint parameters with continue flag
+    params['continue'] = True
 
-# load aBot
-if params['startFrom']:
-    aBot, loadedParams, _ = utils.loadModel(params, 'abot', overwrite=True)
-    assert aBot.encoder.vocabSize == dataset.vocabSize, "Vocab size mismatch!"
-    for key in loadedParams:
-        params[key] = loadedParams[key]
-    aBot.eval()
+    excludeParams = ['batchSize', 'visdomEnv', 'startFrom', 'qstartFrom', 'trainMode', \
+        'evalModeList', 'inputImg', 'inputQues', 'inputJson', 'evalTitle', 'beamSize', \
+        'enableVisdom', 'visdomServer', 'visdomServerPort']
 
-# Retaining certain dataloder parameters
-for key in excludeParams:
-    params[key] = dlparams[key]
+    aBot = None
+    qBot = None
 
-# load qBot
-if params['qstartFrom']:
-    qBot, loadedParams, _ = utils.loadModel(params, 'qbot', overwrite=True)
-    assert qBot.encoder.vocabSize == params[
-        'vocabSize'], "Vocab size mismatch!"
-    for key in loadedParams:
-        params[key] = loadedParams[key]
-    qBot.eval()
+    # load aBot
+    if params['startFrom']:
+        aBot, loadedParams, _ = utils.loadModel(params, 'abot', overwrite=True)
+        assert aBot.encoder.vocabSize == dataset.vocabSize, "Vocab size mismatch!"
+        for key in loadedParams:
+            params[key] = loadedParams[key]
+        aBot.eval()
 
-# Retaining certain dataloder parameters
-for key in excludeParams:
-    params[key] = dlparams[key]
+    # Retaining certain dataloder parameters
+    for key in excludeParams:
+        params[key] = dlparams[key]
 
-print("Using split %s" % params['evalSplit'])
-dataset.split = params['evalSplit']
+    # load qBot
+    if params['qstartFrom']:
+        qBot, loadedParams, _ = utils.loadModel(params, 'qbot', overwrite=True)
+        assert qBot.encoder.vocabSize == params[
+            'vocabSize'], "Vocab size mismatch!"
+        for key in loadedParams:
+            params[key] = loadedParams[key]
+        qBot.eval()
 
-print('Loading json file: ' + params['inputJson'])
-with open(params['inputJson'], 'r') as fileId:
-    info = json.load(fileId)
 
-wordCount = len(info['word2ind'])
-# Add <START> and <END> to vocabulary
-info['word2ind']['<START>'] = wordCount + 1
-info['word2ind']['<END>'] = wordCount + 2
-startToken = info['word2ind']['<START>']
-endToken = info['word2ind']['<END>']
-# Padding token is at index 0
-vocabSize = wordCount + 3
-print('Vocab size with <START>, <END>: %d' % vocabSize)
+    # Retaining certain dataloder parameters
+    for key in excludeParams:
+        params[key] = dlparams[key]
 
-# Construct the reverse map
-info['ind2word'] = {
-    int(ind): word
-    for word, ind in info['word2ind'].items()
-}
+    print("Using split %s" % params['evalSplit'])
+    dataset.split = params['evalSplit']
+
+    print('Loading json file: ' + params['inputJson'])
+    with open(params['inputJson'], 'r') as fileId:
+        info = json.load(fileId)
+
+    wordCount = len(info['word2ind'])
+    # Add <START> and <END> to vocabulary
+    info['word2ind']['<START>'] = wordCount + 1
+    info['word2ind']['<END>'] = wordCount + 2
+    startToken = info['word2ind']['<START>']
+    endToken = info['word2ind']['<END>']
+    # Padding token is at index 0
+    vocabSize = wordCount + 3
+    print('Vocab size with <START>, <END>: %d' % vocabSize)
+
+    # Construct the reverse map
+    info['ind2word'] = {
+        int(ind): word
+        for word, ind in info['word2ind'].items()
+    }
+    return dataset, qBot, aBot, params, info
+
 
 ind_map = lambda words: np.array([info['word2ind'].get(word, info['word2ind']['UNK']) 
-                                  for word in words], dtype='int64')
+                                for word in words], dtype='int64')
 tokenize = lambda string: ['<START>'] + word_tokenize(string) + ['<END>']
 to_str_pred = lambda w, l: str(" ".join([info['ind2word'][x] for x in list( filter(
     lambda x:x>0,w.data.cpu().numpy()))][:l.data.cpu()[0]]))[8:]
 to_str_gt = lambda w: str(" ".join([info['ind2word'][x] for x in filter(
     lambda x:x>0,w.data.cpu().numpy())]))[8:-6]
+
+def fix_tokenization_spaces(text):
+    text = text.replace("' ", "'").replace(" '", "'")
+    text = text.replace("ca n't", "can't").replace(" ?", "?")
+    text = text.replace("do n't", "don't").replace(" ,", ",")
+    text = text.replace("is n't", "isn't")
+    return text
+
 def var_map(tensor):
     if params['useGPU']:
         tensor = tensor.cuda()
@@ -210,11 +242,128 @@ def fetch_a_bot_response(aBot, history, img_features, caption, **generation_kwar
     pred_str = to_str_pred(answers[0], ansLens[0])
     return pred_str
 
+def step(qBot, aBot, history, img_features, caption, **generation_kwargs):
+    a_response = fetch_a_bot_response(aBot, history, img_features, caption, **generation_kwargs)
+    history += [{'speaker': 'answer', 'text': a_response}]
+    reward = fetch_reward(qBot, history, img_features, caption)
+    return a_response, reward
 
-if __name__ == "__main__":
+def model_process():
+    global qBot
+    global aBot
+    global params
+    global info
+    global dataset
+    dataset, qBot, aBot, params, info = load_objects()
+    print('BOTS LOADED!')
+    while True:
+        request_id, f_name, args, kwargs = Q.get()
+        if f_name == 'fetch_a_bot_response':
+            f = partial(fetch_a_bot_response, aBot)
+        elif f_name == 'fetch_q_bot_response':
+            f = partial(fetch_q_bot_response, qBot)
+        elif f_name == 'fetch_reward':
+            f = partial(fetch_reward, qBot)
+        elif f_name == 'step':
+            f = partial(step, qBot, aBot)
+        else:
+            raise NotImplementedError
+        result = f(*args, **kwargs)
+        r.set('result_%d' % (request_id), pkl.dumps(result))
+
+def flask_process():
+    app.run(host='0.0.0.0', port=5000, threaded=True, processes=1)
+
+@app.route('/fetch_a_bot_response', methods=['POST'])
+def flask_fetch_a_bot_response():
+    history = request.form.get('history', None)
+    img_features = request.form.get('img_features', None)
+    caption = request.form.get('caption', None)
+    generation_kwargs = request.form.get('generation_kwargs', None)
+    history = json.loads(history)
+    img_features = torch.tensor(json.loads(img_features))
+    generation_kwargs = json.loads(generation_kwargs)
+
+    request_id = int(r.incr('request_id_counter'))
+    Q.put((request_id, 'fetch_a_bot_response', (history, img_features, caption,), generation_kwargs,))
+    while not r.exists("result_%d" % (request_id)):
+        time.sleep(0.05)
+    
+    result = pkl.loads(r.get("result_%d" % (request_id)))
+    r.delete("result_%d" % (request_id))
+    return json.dumps(fix_tokenization_spaces(result))
+
+@app.route('/fetch_q_bot_response', methods=['POST'])
+def flask_fetch_q_bot_response():
+    history = request.form.get('history', None)
+    caption = request.form.get('caption', None)
+    generation_kwargs = request.form.get('generation_kwargs', None)
+    history = json.loads(history)
+    generation_kwargs = json.loads(generation_kwargs)
+
+    request_id = int(r.incr('request_id_counter'))
+    Q.put((request_id, 'fetch_q_bot_response', (history, caption,), generation_kwargs,))
+    while not r.exists("result_%d" % (request_id)):
+        time.sleep(0.05)
+    
+    result = pkl.loads(r.get("result_%d" % (request_id)))
+    r.delete("result_%d" % (request_id))
+    return json.dumps(fix_tokenization_spaces(result))
+
+@app.route('/fetch_reward', methods=['POST'])
+def flask_fetch_reward():
+    history = request.form.get('history', None)
+    img_features = request.form.get('img_features', None)
+    caption = request.form.get('caption', None)
+    history = json.loads(history)
+    img_features = torch.tensor(json.loads(img_features))
+
+    request_id = int(r.incr('request_id_counter'))
+    Q.put((request_id, 'fetch_reward', (history, img_features, caption,), {},))
+    while not r.exists("result_%d" % (request_id)):
+        time.sleep(0.05)
+    
+    result = pkl.loads(r.get("result_%d" % (request_id)))
+    r.delete("result_%d" % (request_id))
+    return json.dumps(result.item())
+
+@app.route('/step', methods=['POST'])
+def flask_step():
+    history = request.form.get('history', None)
+    img_features = request.form.get('img_features', None)
+    caption = request.form.get('caption', None)
+    generation_kwargs = request.form.get('generation_kwargs', None)
+    history = json.loads(history)
+    img_features = torch.tensor(json.loads(img_features))
+    generation_kwargs = json.loads(generation_kwargs)
+
+    request_id = int(r.incr('step'))
+    Q.put((request_id, 'step', (history, img_features, caption,), generation_kwargs,))
+    while not r.exists("result_%d" % (request_id)):
+        time.sleep(0.05)
+    
+    result = pkl.loads(r.get("result_%d" % (request_id)))
+    r.delete("result_%d" % (request_id))
+    return json.dumps((fix_tokenization_spaces(result[0]), result[1].item(),))
+
+def main():
+    global Q
+    Q = mp.Manager().Queue()
+
+    p = mp.Process(target=flask_process)
+    p.start()
+
+    model_process()
+
+def test():
+    global params
+    global info
+    global dataset
+    dataset, _, _, params, info = load_objects()
+    url = 'http://172.31.10.99:5000/'
     while True:
         item = random.choice(dataset)
-        img_feat = item['img_feat']
+        img_feat = item['img_feat'].tolist()
         caption = to_str_gt(item['cap'])
         data = []
         n_rounds = 10
@@ -223,11 +372,22 @@ if __name__ == "__main__":
         print()
 
         for _ in range(n_rounds):
-            q_response = fetch_q_bot_response(qBot, data, caption, inference='greedy', beamSize=1)
+            q_response = json.loads(requests.post(url+'fetch_q_bot_response', 
+                                                  data={'history': json.dumps(data), 
+                                                        'caption': caption, 
+                                                        'generation_kwargs': json.dumps({'inference': 'greedy', 'beamSize': 1})}).text)
             print('q:', q_response)
             data.append({'speaker': 'question', 'text': q_response})
-            a_response = fetch_a_bot_response(aBot, data, img_feat, caption, inference='greedy', beamSize=1)
+            a_response, reward = json.loads(requests.post(url+'step', 
+                                                          data={'history': json.dumps(data), 
+                                                                'caption': caption, 
+                                                                'img_features': json.dumps(img_feat), 
+                                                                'generation_kwargs': json.dumps({'inference': 'greedy', 'beamSize': 1})}).text)
             print('a:', a_response)
             data.append({'speaker': 'answer', 'text': a_response})
-            print('reward:', fetch_reward(qBot, data, img_feat, caption).item())
+            print('reward:', reward)
         print('='*25)
+
+if __name__ == "__main__":
+    main()
+    # test()
